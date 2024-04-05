e255127ea1c9cdce7909b0f49d54aea13cdae9d5
kenzaES <kenza.esdiri@gmail.com>
2024-04-05 11:31:27 +0000

    Co-authored-by: ADIL RADIDI <ADILRADIDI@users.noreply.github.com>

---
+++ moneySack-main/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.getElementById('menu-toggle');
+  const menu = document.querySelector('.container-hamburger');
+
+  // -->Toggle menu in click my icon
+  menuToggle.addEventListener('click', function () {
+    menu.classList.toggle('show');
+  });
+
+  // -->Close menu
+  document.addEventListener('click', function (event) {
+    if (!menu.contains(event.target) && event.target !== menuToggle) {
+      menu.classList.remove('show');
+    }
+  });
+});
